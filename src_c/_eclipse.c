f1203d93434ef0952fe788bb50dce7a94b3fa3c6
pcubillos <patricio.cubillos@oeaw.ac.at>
2018-04-15 15:08:21 +0200

    Cleaned and documented _eclipse.c source code.

--- src_c/_eclipse.c
+++ src_c/_eclipse.c
@@ -1,3 +1,6 @@
+// Copyright (c) 2018 Patricio Cubillos and contributors.
+// eclipse is open-source software under the MIT license (see LICENSE).
+
 #include<Python.h>
 #define NPY_NO_DEPRECATED_API NPY_1_7_API_VERSION
 #include<numpy/arrayobject.h>
@@ -12,6 +15,16 @@ double sign(double x){
   return 1.0;
 }
 
+double k0(double p, double z){
+  if (p==0)
+    return 0.0;
+  return acos(0.5*(p*p + z*z - 1)/(p*z));
+}
+
+double k1(double p, double z){
+  return acos(0.5*(1 - p*p + z*z)/z);
+}
+
 
 PyDoc_STRVAR(mandelecl__doc__,
 "Secondary-eclipse light-curve model from Mandel & Agol (2002).\n\
@@ -21,10 +34,10 @@ Parameters                                                     \n\
 params: 1D float ndarray                                       \n\
    Eclipse model parameters:                                   \n\
      midpt:  Center of eclipse.                                \n\
-     width:  Eclipse duration between contacts 1 to 4.         \n\
+     width:  Eclipse duration between contacts 1 and 4.        \n\
      depth:  Eclipse depth.                                    \n\
-     t12:    Eclipse duration between contacts 1 to 2.         \n\
-     t34:    Eclipse duration between contacts 3 to 4.         \n\
+     ting:   Eclipse duration between contacts 1 and 2.        \n\
+     tegr:   Eclipse duration between contacts 3 and 4.        \n\
      flux:   Out-of-eclipse flux level.                        \n\
 t: 1D float ndarray                                            \n\
    The lightcurve's phase/time points.                         \n\
@@ -36,8 +49,8 @@ eclipse: 1D float ndarray                                      \n\
 
 static PyObject *mandelecl(PyObject *self, PyObject *args){
   PyArrayObject *t, *eclipse, *params;
-  double midpt, width, depth, t12, t34, flux;
-  double t1, t2, t3, t4, p, z, k0, k1;
+  double midpt, width, depth, ting, tegr, flux;
+  double t1, t2, t3, t4, p, z;
   int i;
   npy_intp dims[1];
 
@@ -48,8 +61,8 @@ static PyObject *mandelecl(PyObject *self, PyObject *args){
   midpt = INDd(params, 0);
   width = INDd(params, 1);
   depth = INDd(params, 2);
-  t12   = INDd(params, 3);
-  t34   = INDd(params, 4);
+  ting  = INDd(params, 3);
+  tegr  = INDd(params, 4);
   flux  = INDd(params, 5);
 
   dims[0] = (int)PyArray_DIM(t, 0);
@@ -65,44 +78,38 @@ static PyObject *mandelecl(PyObject *self, PyObject *args){
   /* Time of contact points: */
   t1 = midpt - width/2;
 
-  if ((t1+t12) < midpt)
-    t2 = t1 + t12;
+  if ((t1+ting) < midpt)
+    t2 = t1 + ting;
   else
     t2 = midpt;
 
   t4 = midpt + width/2;
-  if ((t4-t34) > midpt)
-    t3 = t4 - t34;
+  if ((t4-tegr) > midpt)
+    t3 = t4 - tegr;
   else
     t3 = midpt;
 
-  p = depth/sqrt(fabs(depth));
+  p = sign(depth) * sqrt(fabs(depth));
 
   for(i=0; i<dims[0]; i++){
+    INDd(eclipse,i) = 1.0;
     /* Out of eclipse: */
     if (INDd(t,i) < t1 || INDd(t,i) > t4){
-      INDd(eclipse,i) = 1.0;
+      INDd(eclipse,i) += depth;
     }
     /* Totality:       */
     else if (INDd(t,i) >= t2  &&  INDd(t,i) <= t3){
-      INDd(eclipse,i) = 1 - depth;
     }
     /* Eq. (1) of Mandel & Agol (2002) for ingress/egress:  */
     else if (p != 0){
       if (INDd(t,i) > t1  &&  INDd(t,i) < t2){
-        z  = -2*p*(INDd(t,i)-t1)/t12 + 1 + p;
-        k0 = acos(0.5*(p*p + z*z - 1)/p/z);
-        k1 = acos(0.5*(1 - p*p + z*z)/z);
-        INDd(eclipse,i) = 1 - depth/fabs(depth)/M_PI * (p*p*k0 + k1
-                                    - sqrt((4*z*z - pow((1+z*z-p*p),2))/4));
+        z  = -2*p*(INDd(t,i)-t1)/ting + 1 + p;
       }
       else if (INDd(t,i) > t3  &&  INDd(t,i) < t4){
-        z  = 2*p*(INDd(t,i)-t3)/t34 + 1 - p;
-        k0 = acos((p*p+z*z-1)/2/p/z);
-        k1 = acos((1-p*p+z*z)/2/z);
-        INDd(eclipse,i) = 1-depth/fabs(depth)/M_PI*(p*p*k0 + k1
-                                    - sqrt((4*z*z - pow((1+z*z-p*p),2))/4));
+        z  =  2*p*(INDd(t,i)-t3)/tegr + 1 - p;
       }
+      INDd(eclipse,i) += depth - sign(depth)/M_PI * (p*p*k0(p,z) + k1(p,z)
+                                      - sqrt(z*z - 0.25*pow(1+z*z-p*p,2)));
     }
     INDd(eclipse,i) *= flux;
   }
@@ -117,8 +124,8 @@ independent ingress and egress depths.");
 
 static PyObject *eclipse_flat(PyObject *self, PyObject *args){
   PyArrayObject *t, *eclipse, *params;
-  double midpt, width, idepth, edepth, t12, t34, flux;
-  double t1, t2, t3, t4, pi, pe, z, k0, k1;
+  double midpt, width, idepth, edepth, ting, tegr, flux;
+  double t1, t2, t3, t4, pi, pe, z;
   int i;
   npy_intp dims[1];
 
@@ -130,65 +137,54 @@ static PyObject *eclipse_flat(PyObject *self, PyObject *args){
   width  = INDd(params, 1);
   idepth = INDd(params, 2);  // Ingress depth
   edepth = INDd(params, 3);  // Egress depth
-  t12    = INDd(params, 4);
-  t34    = INDd(params, 5);
+  ting   = INDd(params, 4);
+  tegr   = INDd(params, 5);
   flux   = INDd(params, 6);
 
   dims[0] = (int)PyArray_DIM(t, 0);
   eclipse = (PyArrayObject *) PyArray_SimpleNew(1, dims, NPY_DOUBLE);
 
-  if(idepth == 0){
-      for(i=0; i<dims[0]; i++){
-        INDd(eclipse, i) = flux;
-      }
-      return Py_BuildValue("N", eclipse);
-    }
-
   /* Time of contact points: */
   t1 = midpt - width/2;
-  t2 = t1 + t12;
+  t2 = t1 + ting;
   /* Grazing eclipse:        */
-  if ((t1+t12) > midpt)
+  if ((t1+ting) > midpt)
     t2 = midpt;
 
   t4 = midpt + width/2;
-  t3 = t4 - t34;
-  if ((t4-t34) < midpt)
+  t3 = t4 - tegr;
+  if ((t4-tegr) < midpt)
     t3 = midpt;
 
   /* Rp/Rs at ingress and egress: */
-  pi = idepth/sqrt(fabs(idepth));  /* Not to confuse with pi */
-  pe = edepth/sqrt(fabs(edepth));
+  pi = sign(idepth) * sqrt(fabs(idepth));  /* Not to confuse with pi */
+  pe = sign(edepth) * sqrt(fabs(edepth));
 
   for(i=0; i<dims[0]; i++){
+    INDd(eclipse,i) = 1.0;
     /* Before ingress: */
     if (INDd(t,i) < t1){
-      INDd(eclipse,i) = 1.0 + idepth;
+      INDd(eclipse,i) += idepth;
     }
     /* During ingress:                   */
     /* Eq. (1) of Mandel & Agol (2002):  */
-    else if (INDd(t,i) < t2){
-      z  = -2*pi*(INDd(t,i)-t1)/t12 + 1 + pi;
-      k0 = acos(0.5*(pi*pi + z*z - 1)/pi/z);
-      k1 = acos(0.5*(1 - pi*pi + z*z)/z);
-      INDd(eclipse,i) = 1 + idepth - sign(idepth)/M_PI * (pi*pi*k0 + k1
-                                   - sqrt((z*z - 0.25*pow((1+z*z-pi*pi),2))));
+    else if (INDd(t,i) < t2  &&  pi != 0){
+      z = -2*pi*(INDd(t,i)-t1)/ting + 1 + pi;
+      INDd(eclipse,i) += idepth - sign(idepth)/M_PI * (pi*pi*k0(pi,z) + k1(pi,z)
+                                   - sqrt(z*z - 0.25*pow(1+z*z-pi*pi,2)));
     }
     /* Totality:       */
     else if (INDd(t,i) < t3){
-      INDd(eclipse,i) = 1.0;
     }
     /* During egress:  */
-    else if (INDd(t,i) < t4){
-      z  = 2*pe*(INDd(t,i)-t3)/t34 + 1 - pe;
-      k0 = acos((pe*pe+z*z-1)/2/pe/z);
-      k1 = acos((1-pe*pe+z*z)/2/z);
-      INDd(eclipse,i) = 1 + edepth - sign(edepth)/M_PI*(pe*pe*k0 + k1
-                                   - sqrt((z*z - 0.25*pow((1+z*z-pe*pe),2))));
+    else if (INDd(t,i) < t4  &&  pe != 0){
+      z =  2*pe*(INDd(t,i)-t3)/tegr + 1 - pe;
+      INDd(eclipse,i) += edepth - sign(edepth)/M_PI * (pe*pe*k0(pe,z) + k1(pe,z)
+                                   - sqrt(z*z - 0.25*pow(1+z*z-pe*pe,2)));
     }
     /* After egress:   */
     else if (INDd(t,i) >= t4){
-      INDd(eclipse,i) = 1.0 + edepth;
+      INDd(eclipse,i) += edepth;
     }
     INDd(eclipse,i) *= flux;
   }
@@ -202,8 +198,8 @@ independent ingress and egress depths and slopes.");
 
 static PyObject *eclipse_lin(PyObject *self, PyObject *args){
   PyArrayObject *t, *eclipse, *params;
-  double midpt, width, idepth, edepth, t12, t34, flux, islope, eslope;
-  double t1, t2, t3, t4, pi, pe, z, k0, k1;
+  double midpt, width, idepth, edepth, ting, tegr, flux, islope, eslope;
+  double t1, t2, t3, t4, pi, pe, z;
   int i;
   npy_intp dims[1];
 
@@ -215,8 +211,8 @@ static PyObject *eclipse_lin(PyObject *self, PyObject *args){
   width  = INDd(params, 1);
   idepth = INDd(params, 2);  // Ingress depth
   edepth = INDd(params, 3);  // Egress depth
-  t12    = INDd(params, 4);
-  t34    = INDd(params, 5);
+  ting   = INDd(params, 4);
+  tegr   = INDd(params, 5);
   flux   = INDd(params, 6);
   islope = INDd(params, 7);
   eslope = INDd(params, 8);
@@ -224,58 +220,47 @@ static PyObject *eclipse_lin(PyObject *self, PyObject *args){
   dims[0] = (int)PyArray_DIM(t, 0);
   eclipse = (PyArrayObject *) PyArray_SimpleNew(1, dims, NPY_DOUBLE);
 
-  if(idepth == 0){
-      for(i=0; i<dims[0]; i++){
-        INDd(eclipse, i) = flux;
-      }
-      return Py_BuildValue("N", eclipse);
-    }
-
   /* Time of contact points: */
   t1 = midpt - width/2;
-  t2 = t1 + t12;
+  t2 = t1 + ting;
   /* Grazing eclipse:        */
-  if ((t1+t12) > midpt)
+  if ((t1+ting) > midpt)
     t2 = midpt;
 
   t4 = midpt + width/2;
-  t3 = t4 - t34;
-  if ((t4-t34) < midpt)
+  t3 = t4 - tegr;
+  if ((t4-tegr) < midpt)
     t3 = midpt;
 
   /* Rp/Rs at ingress and egress: */
-  pi = idepth/sqrt(fabs(idepth));  /* Not to confuse with pi */
-  pe = edepth/sqrt(fabs(edepth));
+  pi = sign(idepth) * sqrt(fabs(idepth));  /* Not to confuse with pi */
+  pe = sign(edepth) * sqrt(fabs(edepth));
 
   for(i=0; i<dims[0]; i++){
+    INDd(eclipse,i) = 1.0;
     /* Before ingress: */
     if (INDd(t,i) < t1){
-      INDd(eclipse,i) = 1.0 + islope*(INDd(t,i)-t1) + idepth;
+      INDd(eclipse,i) += idepth + islope*(INDd(t,i)-t1);
     }
     /* During ingress:                   */
     /* Eq. (1) of Mandel & Agol (2002):  */
-    else if (INDd(t,i) < t2){
-      z  = -2*pi*(INDd(t,i)-t1)/t12 + 1 + pi;
-      k0 = acos(0.5*(pi*pi + z*z - 1)/pi/z);
-      k1 = acos(0.5*(1 - pi*pi + z*z)/z);
-      INDd(eclipse,i) = 1 + idepth - sign(idepth)/M_PI * (pi*pi*k0 + k1
-                                   - sqrt((z*z - 0.25*pow((1+z*z-pi*pi),2))));
+    else if (INDd(t,i) < t2  &&  pi != 0.0){
+      z = -2*pi*(INDd(t,i)-t1)/ting + 1 + pi;
+      INDd(eclipse,i) += idepth - sign(idepth)/M_PI * (pi*pi*k0(pi,z) + k1(pi,z)
+                                   - sqrt(z*z - 0.25*pow(1+z*z-pi*pi,2)));
     }
     /* Totality:       */
     else if (INDd(t,i) < t3){
-      INDd(eclipse,i) = 1.0;
     }
     /* During egress:  */
-    else if (INDd(t,i) < t4){
-      z  = 2*pe*(INDd(t,i)-t3)/t34 + 1 - pe;
-      k0 = acos((pe*pe+z*z-1)/2/pe/z);
-      k1 = acos((1-pe*pe+z*z)/2/z);
-      INDd(eclipse,i) = 1 + edepth - sign(edepth)/M_PI*(pe*pe*k0 + k1
-                                   - sqrt((z*z - 0.25*pow((1+z*z-pe*pe),2))));
+    else if (INDd(t,i) < t4  &&  pe != 0.0){
+      z =  2*pe*(INDd(t,i)-t3)/tegr + 1 - pe;
+      INDd(eclipse,i) += edepth - sign(edepth)/M_PI * (pe*pe*k0(pe,z) + k1(pe,z)
+                                   - sqrt(z*z - 0.25*pow(1+z*z-pe*pe,2)));
     }
     /* After egress:   */
     else if (INDd(t,i) >= t4){
-      INDd(eclipse,i) = 1.0 + eslope*(INDd(t,i)-t4) + edepth;
+      INDd(eclipse,i) += edepth + eslope*(INDd(t,i)-t4);
     }
     /* Flux normalization: */
     INDd(eclipse,i) *= flux;
@@ -286,13 +271,35 @@ static PyObject *eclipse_lin(PyObject *self, PyObject *args){
 
 
 PyDoc_STRVAR(eclipse_quad__doc__,
-"Secondary-eclipse light-curve model with independent ingress and\n\
-egress depths and quadratic baseline.");
+"Secondary-eclipse light-curve model with independent ingress and    \n\
+egress depths and quadratic baseline.                                \n\
+                                                                     \n\
+Parameters                                                           \n\
+----------                                                           \n\
+params: 1D float ndarray                                             \n\
+   Eclipse model parameters:                                         \n\
+   - midpt:  Mid-eclipse epoch.                                      \n\
+   - width:  Eclipse duration between 1st and 4th contacts (T14).    \n\
+   - idepth: Normalized ingress eclipse depth.                       \n\
+   - edepth: Normalized egress eclipse depth.                        \n\
+   - ting:   Eclipse ingress duration (between 1st and 2nd contacts).\n\
+   - tegr:   Eclipse egress duration (between 3rd and 4th contacts). \n\
+   - flux:   Stellar flux level (i.e., flux during eclipse).         \n\
+   - slope:  Out-of-eclipse flux linear slope.                       \n\
+   - quad:   Out-of-eclipse flux quadratic term.                     \n\
+t: 1D float ndarray                                                  \n\
+   The lightcurve's phase/time points.                               \n\
+                                                                     \n\
+Returns                                                              \n\
+-------                                                              \n\
+eclipse: 1D float ndarray                                            \n\
+   Mandel & Agol eclipse model evaluated at points t.");
+
 
 static PyObject *eclipse_quad(PyObject *self, PyObject *args){
   PyArrayObject *t, *eclipse, *params;
-  double midpt, width, idepth, edepth, t12, t34, flux, slope, quad;
-  double t1, t2, t3, t4, pi, pe, z, k0, k1;
+  double midpt, width, idepth, edepth, ting, tegr, flux, slope, quad;
+  double t1, t2, t3, t4, pi, pe, z;
   int i;
   npy_intp dims[1];
 
@@ -300,12 +307,13 @@ static PyObject *eclipse_quad(PyObject *self, PyObject *args){
     return NULL;
   }
 
+  /* Unpack params:           */
   midpt  = INDd(params, 0);
   width  = INDd(params, 1);
-  idepth = INDd(params, 2);  // Ingress depth
-  edepth = INDd(params, 3);  // Egress depth
-  t12    = INDd(params, 4);
-  t34    = INDd(params, 5);
+  idepth = INDd(params, 2);
+  edepth = INDd(params, 3);
+  ting   = INDd(params, 4);
+  tegr   = INDd(params, 5);
   flux   = INDd(params, 6);
   slope  = INDd(params, 7);
   quad   = INDd(params, 8);
@@ -313,62 +321,51 @@ static PyObject *eclipse_quad(PyObject *self, PyObject *args){
   dims[0] = (int)PyArray_DIM(t, 0);
   eclipse = (PyArrayObject *) PyArray_SimpleNew(1, dims, NPY_DOUBLE);
 
-  if(idepth == 0){
-      for(i=0; i<dims[0]; i++){
-        INDd(eclipse, i) = flux;
-      }
-      return Py_BuildValue("N", eclipse);
-    }
-
-  /* Time of contact points: */
+  /* Time of contact points:  */
   t1 = midpt - width/2;
-  t2 = t1 + t12;
-  /* Grazing eclipse:        */
-  if ((t1+t12) > midpt)
+  t2 = t1 + ting;
+  /* Grazing eclipse:         */
+  if ((t1+ting) > midpt)
     t2 = midpt;
 
   t4 = midpt + width/2;
-  t3 = t4 - t34;
-  if ((t4-t34) < midpt)
+  t3 = t4 - tegr;
+  if ((t4-tegr) < midpt)
     t3 = midpt;
 
-  /* Rp/Rs at ingress and egress: */
-  pi = idepth/sqrt(fabs(idepth));  /* Not to confuse with pi */
-  pe = edepth/sqrt(fabs(edepth));
+  /* Rp/Rs at ingress and egress:                                    */
+  pi = sign(idepth) * sqrt(fabs(idepth));  /* Not to confuse with pi */
+  pe = sign(edepth) * sqrt(fabs(edepth));
 
-  for(i=0; i<dims[0]; i++){
-    /* Before ingress: */
+  for (i=0; i<dims[0]; i++){
+    INDd(eclipse,i) = 1.0;
+    /* Before ingress:                   */
     if (INDd(t,i) < t1){
-      INDd(eclipse,i) = 1.0 + quad *(INDd(t,i)*INDd(t,i)-t1*t1)
-                            + slope*(INDd(t,i)-t1) + idepth;
+      INDd(eclipse,i) += quad * (INDd(t,i) + t1 - 2*midpt) * (INDd(t,i) - t1)
+                         + slope*(INDd(t,i)-t1) + idepth;
     }
     /* During ingress:                   */
     /* Eq. (1) of Mandel & Agol (2002):  */
-    else if (INDd(t,i) < t2){
-      z  = -2*pi*(INDd(t,i)-t1)/t12 + 1 + pi;
-      k0 = acos(0.5*(pi*pi + z*z - 1)/pi/z);
-      k1 = acos(0.5*(1 - pi*pi + z*z)/z);
-      INDd(eclipse,i) = 1 + idepth - sign(idepth)/M_PI * (pi*pi*k0 + k1
-                                   - sqrt((z*z - 0.25*pow((1+z*z-pi*pi),2))));
+    else if (INDd(t,i) < t2  &&  pi != 0.0){
+      z  = -2*pi*(INDd(t,i)-t1)/ting + 1 + pi;
+      INDd(eclipse,i) += idepth - sign(idepth)/M_PI * (pi*pi*k0(pi,z) + k1(pi,z)
+                                    - sqrt(z*z - 0.25*pow(1+z*z-pi*pi,2)));
     }
-    /* Totality:       */
-    else if (INDd(t,i) < t3){
-      INDd(eclipse,i) = 1.0;
+    /* Totality (t2 < t < t3):           */
+    else if (INDd(t,i) < t3){  /* Already set INDd(eclipse,i) = 1.0 */
     }
-    /* During egress:  */
-    else if (INDd(t,i) < t4){
-      z  = 2*pe*(INDd(t,i)-t3)/t34 + 1 - pe;
-      k0 = acos((pe*pe+z*z-1)/2/pe/z);
-      k1 = acos((1-pe*pe+z*z)/2/z);
-      INDd(eclipse,i) = 1 + edepth - sign(edepth)/M_PI*(pe*pe*k0 + k1
-                                   - sqrt((z*z - 0.25*pow((1+z*z-pe*pe),2))));
+    /* During egress:                    */
+    else if (INDd(t,i) < t4  &&  pe != 0.0){
+      z  = 2*pe*(INDd(t,i)-t3)/tegr + 1 - pe;
+      INDd(eclipse,i) += edepth - sign(edepth)/M_PI * (pe*pe*k0(pe,z) + k1(pe,z)
+                                    - sqrt(z*z - 0.25*pow(1+z*z-pe*pe,2)));
     }
-    /* After egress:   */
+    /* After egress:                     */
     else if (INDd(t,i) >= t4){
-      INDd(eclipse,i) = 1.0 + quad *(INDd(t,i)*INDd(t,i) - t4*t4)
-                            + slope*(INDd(t,i)-t4) + edepth;
+      INDd(eclipse,i) += quad * (INDd(t,i) + t4 - 2*midpt) * (INDd(t,i) - t4)
+                         + slope*(INDd(t,i)-t4) + edepth;
     }
-    /* Flux normalization: */
+    /* Flux normalization:               */
     INDd(eclipse,i) *= flux;
   }
   return Py_BuildValue("N", eclipse);
@@ -376,7 +373,7 @@ static PyObject *eclipse_quad(PyObject *self, PyObject *args){
 
 
 PyDoc_STRVAR(eclipse__doc__,
-             "Eclipse light-curve models for the Webb.\n");
+             "Eclipse light-curve models for the times of the Webb.\n");
 
 
 static PyMethodDef eclipse_methods[] = {
